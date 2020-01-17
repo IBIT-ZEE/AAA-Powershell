1875cb00e92e72364d8da0b379feab0b99a7ec26
IBIT.ZEE <ibit.zee@gmail.com>
2020-01-17 15:54:45 +0000

    20200117a
    bordalima

---
+++ AAA/profile-copy.cmd
@@ -0,0 +1,9 @@
+
+
+
+copy c:\dat\powershell\aaa\profile.ps1 C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 
+
+
+
+pause
+
---
+++ AAA/profile-link.cmd
@@ -0,0 +1,9 @@
+
+
+
+mklink C:\Windows\System32\WindowsPowerShell\v1.0\profile.ps1 c:\dat\powershell\aaa\profile.ps1
+
+
+
+pause
+
